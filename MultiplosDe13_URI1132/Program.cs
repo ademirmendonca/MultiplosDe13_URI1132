7ff97cca6c310c83bbe5eb8f475766568c6ffad2
Ademir Mendonça <ademir_mendonca@hotmail.com>
2021-06-23 12:18:39 -0300

    .

---
+++ MultiplosDe13_URI1132/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MultiplosDe13_URI1132
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            
+            int menor = x;
+            int maior = y;
+
+            //Verifica as condições entre maior e menor.
+            if (x > y)
+            {
+                menor = y;
+                maior = x;
+            }
+
+                int total = 0;
+           
+                //Calcula os valores um a um 
+                for (int i = menor; i <= maior; i++)
+                {
+
+                    //Verifica os valores que não são divisíveis por 13
+                    if (i % 13 != 0)
+                    {
+                    total = total + i;
+                    }                    
+                }
+
+            Console.WriteLine(total);
+
+        }
+    }
+}
